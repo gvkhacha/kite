955a5aa81e39580ee010fee5524f672787fcf848
Garik Khachatryan <gvkhacha@uci.edu>
2018-05-13 13:46:43 -0700

    tokenizing html

--- build_index.py
+++ build_index.py
@@ -3,11 +3,35 @@ from collections import defaultdict
 
 import interact_files
 
+import lxml.html.diff
+
 import sys
 
 
+
+def tokenizeDoc(doc: DocID) -> [(str, int)]:
+	""" Reads document's file and parses through tokens
+	For now, only considers occurances
+	Return: [(token, occurances)]
+	"""
+	htmlFile = None
+	try:
+		htmlFile = open(doc.getPath())
+		print(lxml.html.diff.tokenize(htmlFile.read()))
+	except:
+		raise #Not sure yet... re-raise
+	finally:
+		if htmlFile:
+			htmlFile.close()
+
+
+
+
+
 def main(index: dict):
-	print(index)
+	for l in interact_files.readFromBook():
+		x = DocID(l)
+		tokenizeDoc(x)
 
 
 if __name__ == '__main__':
--- interact_files.py
+++ interact_files.py
@@ -3,7 +3,7 @@ import pickle, os
 
 # Bookkeeping has path and URL for documents
 BOOKDIR = 'WEBPAGES_RAW/bookkeeping.json'
-TMP = 10 #Change to -1 when done testing
+TMP = 2 #Change to -1 when done testing
 
 
 def readFromBook():
