1f5bedeb190d36c882c1be93e9e9c83e744883ef
Garik Khachatryan <gvkhacha@uci.edu>
2018-05-17 18:56:00 -0700

    stats/simple search for milestone 1

---
+++ generate_report.py
@@ -0,0 +1,44 @@
+"""
+This module will create a report on runtime with the current database found with index.pickle.
+
+ - It will observe the number of documents indexed (As found with a special analytics key placed in the index file)
+ - It will provide other basic analytics for the actual index
+ - It will use the search module and make some searches based on exact matches.
+ 	* There is no canonicalization in the search module
+"""
+
+import simple_search as search
+import interact_files
+
+import os #To get size of index
+
+def generateStats(index: dict):
+	print("Size of Index: {:.2f} KB".format(os.stat('index.pickle').st_size/1024))
+	print("# of Docs: {}".format(index['& LAST DOC &']))
+	print("# of Tokens: {}".format(len(index.keys())-1)) # 1 is stats ^^
+
+
+def searchIndex(index: dict, docs: dict, queries: [str]) -> None:
+	for q in queries:
+		print("="*36)
+		print("{:^36}".format("Search for " + q))
+		print("="*36)
+		counter = 1
+		results = search.query(index, q)
+		for r in results:
+			print("{}. DocID: {}, Score: {}".format(counter, r[0], r[1]))
+			counter += 1
+			print("\t{}".format(docs['/'.join(r[0])]), end="\n\n")
+
+
+def main():
+	index = interact_files.loadIndexFromFile('main')
+	docs = search.getDocIDs()
+	queries = ['Informatics', 'Mondego', 'Irvine', 'artificial intelligence', 'computer science']
+
+	generateStats(index)
+	searchIndex(index, docs, queries)
+
+
+if __name__ == '__main__':
+	main()
--- simple_search.py
+++ simple_search.py
@@ -2,8 +2,17 @@ import interact_files
 from structures import DocID
 import json
 
-def query(index: dict, q: str):
-	return index[q].most_common()[:5]
+
+def query(index: dict, query: str, amount=10) -> [{'docid': int}]:
+	""" TEMP: only searches index by exact matches on query. Does not canonicalize
+	or use operators.
+	Returns first `amount` results"""
+	try:
+		q = query.lower()
+		return index[q].most_common()[:amount]	
+	except IndexError:
+		# Results are less than amount, just give all. 
+		return index[q].most_common()
 
 def getDocIDs() -> dict:
 	with open('bookkeeping.json', 'r') as file:
@@ -16,10 +25,13 @@ def main():
 
 
 	q = input("Give me a search query: ")
-	results = query(index, q)
-	for r in results:
-		print(r)
-		print(docs['/'.join(r[0])])
+	while q != '':
+		results = query(index, q)
+		for r in results:
+			print(r)
+			print(docs['/'.join(r[0])])
+		print('--------------')
+		q = input("Give me a search query: ")
 
 
 if __name__ == '__main__':
