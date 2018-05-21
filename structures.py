9249792feffee1e1a2ffdf987e97d45dd6d25a88
Garik Khachatryan <gvkhacha@uci.edu>
2018-05-21 12:14:11 -0700

    structures annotating

--- structures.py
+++ structures.py
@@ -1,9 +1,9 @@
 from bs4 import BeautifulSoup, Comment
-from nltk.stem.wordnet import WordNetLemmatizer
+# from nltk.stem.wordnet import WordNetLemmatizer
 
 import re
 
-lmtzr = WordNetLemmatizer() #Not sure, but probably better to init once.
+# lmtzr = WordNetLemmatizer() #Not sure, but probably better to init once.
 
 class DocID:
 	def __init__(self, bookentry: str):
@@ -31,19 +31,26 @@ class Tokenizer:
 		with open(self._doc.getPath()) as htmlFile:
 			self._soup = BeautifulSoup(htmlFile, 'lxml')
 		self._getDocData()
+		#self._title, self._comments, self._rootURL
 
-	def findAllTokens(self):
+	def findAllTokens(self) -> None:
+		""" Walks through all HTML elements in the document,
+		checks: Comment -> skip
+				Image   -> add to Img Index
+				Text    -> determine weight -> add to list
+		"""
 		for element in self._soup.descendants:
 			if element.name == None:
 				if element in self._comments:
-					# print("Found a comment!")
+					# Ignore comments
 					pass
 				else:
 					self._determineText(element)
 			elif element.name == 'img':
 				self._addImage(element)
 
-	def _getDocData(self):
+	def _getDocData(self) -> None:
+		""" Part of init. Determines documents comments, titles, and URL"""
 		self._comments = self._soup.find_all(string=lambda text:isinstance(text,Comment))
 		self._title = self._soup.title
 		if self._title != None:
@@ -53,29 +60,35 @@ class Tokenizer:
 			root = root[:-1]
 		self._rootURL = '/'.join(root) + '/'	
 
-	def _addImage(self, element):
+	def _addImage(self, element) -> None:
+		""" Element starts with <img> tag -> Finds details and adds 
+		them to Img Index"""
 		try:
 			src = element['src']
 		except KeyError:
 			#For some reason, img doesn't have src attr *cough* WICS *cough*
 			return
+
 		alt = element.get('alt')
 		if alt == None:
 			alt = ''
-		alt = alt.split('•')[0]
+		alt = alt.split('•')[0] # Tries to consider <img> made from snippets
 		key = (self._title, alt)
 
+		# Fix src url, considers relative and external links
+		# NOT CONSIDERING PARENTS YET (?)
 		if src.startswith('/'):
 			srcURL = self._rootURL.split('/')[0] + element['src']
 		elif 'http' in src:
 			srcURL = src
 		else:
 			srcURL = self._rootURL + element['src']
+
 		val = (srcURL, len(list(element.parents)) * 0.22)
 		if key not in self._imgIndex:
 			self._imgIndex[key].append(val)
 
-	def _determineText(self, element):
+	def _determineText(self, element) -> None:
 		"""Determine the weight of the text found, send to another method to 
 		add to dictionary"""
 		parents = list(element.parents)
@@ -95,7 +108,7 @@ class Tokenizer:
 
 		self._addTextToIndex(element, weight)
 
-	def _addTextToIndex(self, text, weight):
+	def _addTextToIndex(self, text, weight) -> None:
 		"""Takes the text, tokenizes with language changes and number differences"""
 		matches = re.findall(r'\w+', text.lower())
 		for i in range(len(matches)):
