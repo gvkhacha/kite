1e02aceeb7d0b71d5e89d9b61ea222eb13b3d5db
Garik Khachatryan <gvkhacha@uci.edu>
2018-05-22 15:44:10 -0700

    attempting to insert into db

--- optimize_index.py
+++ optimize_index.py
@@ -1,28 +1,57 @@
-import interact_files
-from structures import DocID
+from database import interact_files
 from collections import defaultdict, Counter
-import json, pickle
+from structures import DocID
 
-def getDocIDs() -> dict:
-	with open('bookkeeping.json', 'r') as file:
-		return json.loads(file.read())
+import math, sqlite3
+
+
+def insertIntoDb(data: list, cursor) -> None:
+	""" Takes data and inserts into 'occurances' table of
+	database (SQL) """
+	# for i in data:
+	# 	print(i)
+	# 	print(type(i))
+	cursor.executemany("INSERT INTO occurances VALUES (?, ?, ?)", data)
+
+	
+
+def calculateWeight(token: str, values: dict, numDocs: int) -> None:
+	""" Looking at a single token, and values already have
+	term frequencies: calculates weight of each document 
+	based on normalized term frequency and inverse document
+	frequency
+	Formula (weight) = (1 + log(tf)) * (log(N/df))
+		N = # of total documents
+		df = # of documents token appears in """
+	data = []
+
+	docFreq = len(values)
+	for doc, tf in values.items():
+		weight = 1 + math.log(tf, 10)
+		weight *= math.log(numDocs/docFreq, 10)
+		x, y = doc.getID()
+		docToString = "{}/{}:{}".format(x, y, doc.getURL())
+		data.append( (token, docToString, weight) )
+	return data
+
 
 def main():
-	index = interact_files.loadIndexFromFile('main')
-	docs = getDocIDs()
-	newIndex = defaultdict(Counter)
-	for k, v in index.items():
-		try:
-			for doc, freq in v.items():
-				dstring = "{}/{}".format(doc[0], doc[1])
-				s = '    "{}": "{}"'.format(dstring, docs[dstring])
-				x = DocID(s)
-				newIndex[k][x] += freq
-		except AttributeError:
-			pass
-	with open('index_docID.pickle', 'wb') as file:
-		pickle.dump(newIndex, file, protocol=pickle.HIGHEST_PROTOCOL)
+	conn = sqlite3.connect('index.sqlite')
+	c = conn.cursor()
 
+
+	index = interact_files.loadIndexFromFile('main')
+	numDocs = index['& LAST DOC &']
+
+	count = 0
+	for k, v in index.items():
+		if count > 1000:
+			break
+		count += 1
+		if isinstance(v, dict):
+			data = calculateWeight(k, v, numDocs)
+			insertIntoDb(data, c)
+	c.close()
 
 if __name__ == '__main__':
 	main()
