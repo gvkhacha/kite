49dffbf34033f73dd859513fc47f0b00e59ef9e4
Garik Khachatryan <gvkhacha@uci.edu>
2018-05-22 19:52:04 -0700

    single word query using sqlite

--- simple_search.py
+++ simple_search.py
@@ -1,55 +1,41 @@
-from database import interact_files
-from structures import DocID
+import sqlite3
+from nltk.stem.wordnet import WordNetLemmatizer
 
-import json
-from collections import Counter
-
-
-def searchIndex(index: dict, query: str, amount=10) -> [{'docid': int}]:
-	""" TEMP: only searches index by exact matches on query. Does not canonicalize
-	or use operators.
-	Returns first `amount` results"""
-	q = query.lower()
-	if ' ' in q:
-		combined = []
-		for i in q.split():
-			combined.extend(searchIndex(index, i))
-		# Add all results, then get the best of the results
-		results = Counter()
-		for k, v in sorted(combined):
-			results[k] += v
+lmtzr = WordNetLemmatizer() #Not sure, but probably better to init once.
 
 
-		try:
-			return results.most_common()[:amount]
-		except IndexError:
-			return results.most_common()
-	else:
-		try:
-			return index[q].most_common()[:amount]	
-		except IndexError:
-			# Results are less than amount, just give all. 
-			return index[q].most_common()
 
-def getDocIDs() -> dict:
-	with open('bookkeeping.json', 'r') as file:
-		return json.loads(file.read())
+def resultGenerator(query: str, c, stop=10):
+	""" Generator that will fetch another row on a given query.
+	Query should already be stripped/lowercase/lemmatized/etc
+	* currently only works with one-word queries"""
+	q = tuple(query.split())
+	c.execute('SELECT * FROM occurances WHERE token=? ORDER BY weight DESC', q)
+	for _ in range(stop, 0, -1):
+		yield c.fetchone()
+
+def getQueryInput() -> str:
+	""" Gets string input from user to use as query - makes all
+	changes as needed to get ready for searching """
+	q = input("Input Query: ").strip().lower()
+	return lmtzr.lemmatize(q)
+
+def search(c):
+	q = getQueryInput()
+	while q != '':
+		for i in resultGenerator(q, c):
+			print(i)
+		q = getQueryInput()
+
 
 
 def main():
-	index = interact_files.loadIndexFromFile('main')
+	conn = sqlite3.connect('index.sqlite')
+	c = conn.cursor()
 
+	search(c)
 
-	q = input("Give me a search query: ")
-	while q != '':
-		results = searchIndex(index, q)
-		for r in results:
-			doc = r[0]
-			print(doc.getID())
-			print(doc.getURL())
-		print('--------------')
-		q = input("Give me a search query: ")
-
+	conn.close()
 
 if __name__ == '__main__':
 	main()
