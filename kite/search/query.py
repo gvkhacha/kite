3652482ea76234351d4a5386bccc31e7188cd167
Garik Khachatryan <gvkhacha@uci.edu>
2018-05-24 16:28:23 -0700

    stripping/formatting data

--- kite/search/query.py
+++ kite/search/query.py
@@ -1,12 +1,30 @@
 from django.db import connection
 
-def searchIndex(query: str) -> list:
-	""" Takes string query (generally from input)
-	makes operations on them to be able to index sqlite
-	"""
 
+def _getResultFromIndex(query: str) -> list:
+	""" Return raw result sqlite database """
 	q = tuple(query.strip().split())
 	with connection.cursor() as cursor:
 		cursor.execute('SELECT * FROM occurances WHERE token=%s ORDER BY weight DESC', q)
 		return cursor.fetchmany(10)
-	return []
+
+def _formatResults(raw: list) -> list:
+	""" Taking raw results from database, format the data
+	into usable format for printing 
+	raw: [('token', 'ID:URL', WEIGHT)]
+	result: [(ID, URL, TITLE)]"""
+	results = []
+	for tok, doc, idf in raw:
+		docid, url = doc.split(':')
+		if not url.startswith('http'):
+			url = 'http://' + url
+		results.append( (docid, url) )
+	return results
+
+
+
+def searchIndex(query: str) -> list:
+	""" Takes string query (generally from input)
+	makes operations on them to be able to index sqlite
+	"""
+	return _formatResults(_getResultFromIndex(query))
