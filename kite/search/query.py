48bce2b1ae1e1269dcd185cc34344accacb34365
Garik Khachatryan <gvkhacha@uci.edu>
2018-05-24 16:15:58 -0700

    basic indexing/printing

--- kite/search/query.py
+++ kite/search/query.py
@@ -1,8 +1,12 @@
+from django.db import connection
 
 def searchIndex(query: str) -> list:
 	""" Takes string query (generally from input)
 	makes operations on them to be able to index sqlite
 	"""
-	results = []
-	results.append(query)
-	return results #temporarily, just put query into results.
+
+	q = tuple(query.strip().split())
+	with connection.cursor() as cursor:
+		cursor.execute('SELECT * FROM occurances WHERE token=%s ORDER BY weight DESC', q)
+		return cursor.fetchmany(10)
+	return []
