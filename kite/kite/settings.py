393ca7046607737ad7802aa7afa4509d6740c6e5
Garik Khachatryan <gvkhacha@uci.edu>
2018-05-24 15:51:17 -0700

    simple search send POST

--- kite/kite/settings.py
+++ kite/kite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'search.apps.SearchConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'kite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'kite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'index.sqlite'),
     }
 }
 
--- kite/search/views.py
+++ kite/search/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, render_to_response
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
-from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+@csrf_exempt
+def search(request):
+	if request.POST:
+		print(request.POST['term'])
+		return HttpResponseRedirect("/")
+	else:
+		return render_to_response('search.html')
